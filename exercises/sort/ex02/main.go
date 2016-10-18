f6706f57d1f02ff2c1745ee9939f0a07b5a74c98
Michael Bui <bui.michael652@gmail.com>
2016-10-18 09:56:40 -0400

    New
    New Code

---
+++ exercises/sort/ex02/main.go
@@ -0,0 +1,15 @@
+package main
+
+import (
+	"fmt"
+	"sort"
+)
+
+func main() {
+	s := []string{"B", "Q", "A", "F", "G"}
+	n := []int{2, 54, 2, 5, 7, 345, 3, 45}
+	fmt.Println(s, n)
+	sort.Sort(sort.StringSlice(s))
+	sort.Sort(sort.IntSlice(n))
+	fmt.Println(s, n)
+}
